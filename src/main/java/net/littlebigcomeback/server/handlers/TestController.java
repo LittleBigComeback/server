c2ea8df2493de16af8720bb417c791ff4eb0cc71
xFN10x <mathdudex09@gmail.com>
2026-05-09 19:02:22 -0300

    seperate all mappings

--- src/main/java/net/littlebigcomeback/server/handlers/TestController.java
+++ src/main/java/net/littlebigcomeback/server/handlers/TestController.java
@@ -7,7 +7,35 @@ import org.springframework.web.bind.annotation.*;
 public class TestController {
 
     @GetMapping("*")
-    public String printReq(RequestEntity<String> req) {
+    public String printReqGet(RequestEntity<String> req) {
+        //we should switch to log4j at some point
+        System.out.println("Got a "+ req.getMethod() +" request: " + req.getUrl() + " : " + req.getBody() + " : \n\n" + req.getHeaders() + "\n");
+        return "test";
+    }
+
+    @PutMapping("*")
+    public String printReqPut(RequestEntity<String> req) {
+        //we should switch to log4j at some point
+        System.out.println("Got a "+ req.getMethod() +" request: " + req.getUrl() + " : " + req.getBody() + " : \n\n" + req.getHeaders() + "\n");
+        return "test";
+    }
+
+    @PostMapping("*")
+    public String printReqPost(RequestEntity<String> req) {
+        //we should switch to log4j at some point
+        System.out.println("Got a "+ req.getMethod() +" request: " + req.getUrl() + " : " + req.getBody() + " : \n\n" + req.getHeaders() + "\n");
+        return "test";
+    }
+
+    @PatchMapping("*")
+    public String printReqPatch(RequestEntity<String> req) {
+        //we should switch to log4j at some point
+        System.out.println("Got a "+ req.getMethod() +" request: " + req.getUrl() + " : " + req.getBody() + " : \n\n" + req.getHeaders() + "\n");
+        return "test";
+    }
+
+    @DeleteMapping("*")
+    public String printReqDelete(RequestEntity<String> req) {
         //we should switch to log4j at some point
         System.out.println("Got a "+ req.getMethod() +" request: " + req.getUrl() + " : " + req.getBody() + " : \n\n" + req.getHeaders() + "\n");
         return "test";
