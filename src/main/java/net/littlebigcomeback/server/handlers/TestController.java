a4fa23405b2f039acdbc624d8e9fe12ea86e59b2
xFN10x <mathdudex09@gmail.com>
2026-05-09 18:55:12 -0300

    fix mapping

--- src/main/java/net/littlebigcomeback/server/handlers/TestController.java
+++ src/main/java/net/littlebigcomeback/server/handlers/TestController.java
@@ -6,9 +6,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class TestController {
 
-    @PostMapping("*")
-    @DeleteMapping("*")
-    @PatchMapping("*")
     @GetMapping("*")
     public String printReq(RequestEntity<String> req) {
         //we should switch to log4j at some point
